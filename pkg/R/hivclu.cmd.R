a197427b8f12527cf97142ff3d96715ce8a4abcd
olli0601 <oliver.ratmann@ic.ac.uk>
2013-04-23 17:32:37 +0100

    inital commit

---
+++ pkg/R/hivclu.cmd.R
@@ -0,0 +1,65 @@
+PR.CLUSTALO	<<- "clustalo"
+HPC.NPROC	<<- 1
+HPC.SYS		<<- "CX1"
+HPC.LOAD	<<- "module load intel-suite/10.0 R/2.13.0"
+
+#generate clustalo command
+hiv.cmd.clustalo<- function(indir, infiles, signat=paste(strsplit(date(),split=' ')[[1]],collapse='_',sep=''), outdir=indir, prog= PR.CLUSTALO, nproc=HPC.NPROC)
+{
+	lapply(infiles,function(x)
+			{
+				cat(paste("\nprocess",x,"\n"))				
+				#verbose stuff
+				cmd<- "\t\t\t\t\t\t#######################################################
+					   # run clustalo
+					   #######################################################"
+				cmd<- paste(cmd,paste("\necho \'run ",PR.CLUSTALO,"\'\n",sep=''))
+				
+				#default commands
+				cmd<- paste(cmd,PR.CLUSTALO,sep=" ")								
+				cmd<- paste(cmd, " --infmt=fa --outfmt=fa --force --threads=",nproc," ", sep='' )
+				#file in/out
+				tmp<- paste(indir,x,sep='/')
+				cmd<- paste(cmd, paste("--in",tmp,sep=' ') )				
+				tmp<- paste(outdir,paste(x,signat,"clustalo",sep='.'),sep='/')
+				cmd<- paste(cmd, paste("--out",tmp,sep=' ') )
+				
+				#verbose stuff
+				cmd<- paste(cmd,paste("\necho \'end ",PR.CLUSTALO,"\'\n\n",sep=''))
+				cmd
+			})
+}
+
+#add additional high performance computing information 
+hiv.cmd.hpcwrapper<- function(cmd, hpc.sys=HPC.SYS, hpc.walltime=24, hpc.select= "1:ncpus=1:mem=400mb", hpc.q=NA)
+{
+	wrap<- "#!/bin/sh"
+	if(hpc.sys=="CX1")
+	{
+		tmp	<- paste("#PBS -l walltime=",hpc.walltime,":59:59,pcput=",hpc.walltime,":45:00",sep='')
+		wrap<- paste(wrap, tmp, sep='\n')
+		tmp	<- paste("#PBS -l select=",hpc.select,sep='')
+		wrap<- paste(wrap, tmp, sep='\n')
+		wrap<- paste(wrap, "#PBS -j oe", sep='\n')
+		if(!is.na(hpc.q))
+			wrap<- paste(wrap, paste("#PBS -q",hpc.q), sep='\n\n')
+		wrap<- paste(wrap, HPC.LOAD, sep='\n\n')
+
+	}
+	else
+		stop(paste("unknown hpc.sys",hpc.sys))
+	
+	cmd<- lapply(seq_along(cmd),function(i){	paste(wrap,cmd[[i]],sep='\n')	}) 
+	cmd	
+}
+
+#create high performance computing qsub file and submit
+hiv.cmd.hpccaller<- function(outdir, outfile, cmd)
+{
+	file<- paste(outdir,outfile,sep='/')
+	cat(paste("\nwrite cmd to",file))
+	cat(cmd,file=file)
+	cmd<- paste("qsub",file)
+	cat( cmd )
+	cat( system(cmd, intern=TRUE) )	
+}
---
+++ pkg/misc/hivclu.prjcts.R
@@ -0,0 +1,88 @@
+project.hivc.clustalo<- function()
+{	
+	min.seq.len	<- 21
+	dir.name	<- "/Users/Oliver/duke/2013_HIV_NL/ATHENA_2013"
+	if(0)
+	{
+		#read csv data file and preprocess		
+		verbose		<- 1
+		analyze		<- 1
+		
+		file		<- paste(dir.name,"derived/ATHENA_2013_03_Sequences.csv",sep='/')
+		df			<- read.csv(file, stringsAsFactors=FALSE)		
+		df.names	<- paste("GeneCode",unique(df[,"GeneCode"]),sep='')
+		df			<- lapply(unique(df[,"GeneCode"]),function(gene)
+						{
+							cat(paste("\nprocess GeneCode", gene))
+							tmp<- df[ df[,"GeneCode"]==gene, c("SampleCode","Sequence"), drop=0 ]
+							cat(paste("\nfound n=", nrow(tmp)))
+							seq.len		<- nchar(tmp[,"Sequence"])
+							seq.nok.idx	<- which(seq.len<min.seq.len)
+							seq.ok.idx	<- which(seq.len>=min.seq.len)
+							if(verbose)	cat(paste("\ndiscarding sequences with insufficient length, n=",length(seq.nok.idx),'\n'))
+							if(verbose)	cat(paste("\ndiscarding sequence with insufficient length, SampleCode",paste(tmp[seq.nok.idx,"SampleCode"],collapse=', ')))
+							tmp			<- tmp[seq.ok.idx,]
+							if(verbose) cat(paste("\nfinal n=", nrow(tmp)))
+							tmp
+						})		
+		names(df)	<- df.names
+		file		<- paste(dir.name,"tmp/ATHENA_2013_03_Sequences.R",sep='/')		
+		save(df, file=file)
+	}
+	if(0)
+	{
+		#generate fasta files per gene
+		verbose<- 1
+				
+		file<- paste(dir.name,"tmp/ATHENA_2013_03_Sequences.R",sep='/')
+		load(file)		
+		lapply(seq_along(df),function(i)
+				{
+					df.gene<- df[[i]]
+					df.gene[,"SampleCode"]	<- paste('>',df.gene[,"SampleCode"],sep='')
+					seq.len		<- nchar(df.gene[,"Sequence"])	
+					if(analyze)
+					{
+						cat(paste("\nnumber of sequences:",nrow(df.gene),"\nnumber of unique SampleCodes:", length(unique(df.gene[,"SampleCode"])),"\n") )
+						hist(seq.len, breaks=100)	
+						if(nrow(df.gene)!=length(unique(df.gene[,"SampleCode"])))
+							o<- sapply(unique(df.gene[,"SampleCode"]),function(x)
+									{
+										tmp<- which(df.gene[,"SampleCode"]==x)
+										if(length(tmp)>1)
+										{ 
+											cat("\n found duplicates\n")
+											print(df.gene[tmp,])	
+										}
+									})
+						seq.nok.idx	<- which(seq.len<min.seq.len)
+						if(verbose)	cat(paste("discarding sequences with insufficient length, n=",length(seq.nok.idx),'\n'))			
+					}
+					tmp			<- as.vector(t(df.gene))
+					tmp			<- paste(tmp,collapse='\n')					
+					file		<- paste("derived/ATHENA_2013_03_Sequences_",names(df)[i],".fa",sep='')
+					cat(paste("\nwrite file to",file))
+					cat(tmp,file=file)	
+				})			
+	}
+	if(1)
+	{
+		#generate clustalo command		 
+		indir	<- paste(dir.name,"derived",sep='/')
+		outdir	<- paste(dir.name,"tmp",sep='/')
+		infiles	<- list.files(path=indir, pattern=".fa$", full.names=0)		
+		signat	<- paste(strsplit(date(),split=' ')[[1]],collapse='_',sep='')
+		cmd		<- hiv.cmd.clustalo(indir, infiles, signat=signat, outdir=outdir)
+		cmd		<- hiv.cmd.hpcwrapper(cmd)
+		
+		#cat(cmd)
+		lapply(cmd,function(x)
+				{
+					signat	<- paste(strsplit(date(),split=' ')[[1]],collapse='_',sep='')
+					outfile	<- paste("clustalo",signat,"qsub",sep='.')					
+					hiv.cmd.hpccaller(outdir, outfile, x)			
+				})
+		
+		
+	}
+}
