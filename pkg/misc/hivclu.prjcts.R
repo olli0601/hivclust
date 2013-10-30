181a4c4ba4531c38c0adaba3ceb7a549375c5318
olli0601 <oliver.ratmann@ic.ac.uk>
2013-10-30 08:28:24 +0000

    modified copy of results in examl.bootstrap

--- pkg/misc/hivclu.prjcts.R
+++ pkg/misc/hivclu.prjcts.R
@@ -4420,8 +4420,8 @@ hivc.prog.recombination.check.candidates<- function()
 	id			<- 1
 	seq.select.n<- 15
 	bs.from		<- 0
-	bs.to		<- 499
-	bs.n		<- 500
+	bs.to		<- 2
+	bs.n		<- 3
 	
 	hpc.walltime<- 24
 	hpc.mem		<- "600mb"
