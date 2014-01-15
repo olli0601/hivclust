085ef596d9e449bc6c71ccfb1a75f5c969b6f49c
olli0601 <oliver.ratmann@ic.ac.uk>
2014-01-15 10:38:38 +0000

    also forgot to update sampling prior

--- pkg/misc/hivclu.prjcts.R
+++ pkg/misc/hivclu.prjcts.R
@@ -5907,15 +5907,14 @@ hivc.prog.BEAST2.generate.xml<- function()
 		beast2.spec		<- hivc.beast2.get.specifications(mcmc.length=beast.mcmc.length, bdsky.intervalNumber=5, alignment.filter=alignment.filter)
 		beast2.spec$bdsky.sprop.changepoint.value	<- beast2.spec$bdsky.R0.changepoint.value		<- beast2.spec$bdsky.notInf.changepoint.value	<- c(9.596, 5.596, 1.596, 0.596, 0.)
 		beast2.spec$bdsky.sprop.value				<- c(0.1, 0.5, 0.9, 0.6, 0.3)
-		beast2.spec$bdsky.sprop.prior				<- c("Exponential/0.01/0","Exponential/0.1/0","Uniform/0.2/1.0","Beta/4.0/3.0/0","Beta/2.5/4.0/0")
+		#beast2.spec$bdsky.sprop.prior				<- c("Exponential/0.01/0","Exponential/0.1/0","Uniform/0.2/1.0","Beta/4.0/3.0/0","Beta/2.5/4.0/0")
+		beast2.spec$bdsky.sprop.prior				<- c("Exponential/0.01/0","Exponential/0.1/0","Beta/4.0/3.0/0","Beta/4.0/3.0/0","Beta/2.5/4.0/0")
 		beast2.spec$bdsky.notInf.value				<- 1/c(5, 4, 4, 3, 3)
 		#beast2.spec$bdsky.notInf.prior				<- c("Gamma/5/0.03/0.1","Gamma/5/0.03/0.1","Exponential/0.25/0","Gamma/5/0.05/0.1","Exponential/0.33/0")
 		beast2.spec$bdsky.notInf.prior				<- c("LogNormal/0.2/0.6/0.1/true","LogNormal/0.25/0.8/0.1/true","LogNormal/0.3/0.8/0.1/true","LogNormal/0.5/1.2/0.1/true","LogNormal/0.5/1.2/0.1/true")
 		beast2.spec$sasky.r.prior					<- as.numeric(substr(infilexml.opt,5,nchar(infilexml.opt))) / 100
 		beast2.spec$sasky.r.value					<- rep( beast2.spec$sasky.r.prior / 2, 5 )
 		beast2.spec$sasky.r.prior					<- rep( paste("Uniform/0.0/",beast2.spec$sasky.r.prior,sep=''), 5 )
-		print(beast2.spec$sasky.r.prior)
-		stop()
 	}
 	else if(grepl("ori",infilexml.opt))
 	{
